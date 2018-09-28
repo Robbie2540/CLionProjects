9538fe6739664d2e497e2798bd7d45ed652f128d
Robbie Bartels <Robbiebartels2540@gmail.com>
2018-09-28 17:02:40 -0400

    Initial commit

---
+++ collaboration/cmake-build-debug/CMakeFiles/collaboration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/collaboration.dir/main.cpp.o"
+  "collaboration.pdb"
+  "collaboration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/collaboration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ collaboration/cmake-build-debug/CMakeFiles/collaboration.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for collaboration.
+# This may be replaced when dependencies are built.
