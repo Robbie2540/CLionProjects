1c65efa687ef3f87f3f744ffcef58504db29e1af
technicolor1 <gunsofwar999@gmail.com>
2018-09-29 16:08:57 -0400

    input check 1

--- collaboration/main.cpp
+++ collaboration/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 // characters
 class Human {
    public:
-   int Hp;
-   int Karma;
+   int Hp{};
+   int Karma{};
    vector<string> inventory;
 };
 
@@ -25,7 +25,7 @@ class Weapon {
 };
 
 int main() {
-   Wolf Wolf_lvl1;
+   Wolf Wolf_lvl1{};
    Wolf_lvl1.attack_Power = 100;
    Wolf_lvl1.Hp = 200;
 
@@ -33,12 +33,12 @@ int main() {
    Human mainChar;
    mainChar.Hp = 100;
    mainChar.Karma = 0;
-   mainChar.inventory.push_back("Shirt");
-   mainChar.inventory.push_back("Pants");
-   mainChar.inventory.push_back("Shoes");
+   mainChar.inventory.emplace_back("Shirt");
+   mainChar.inventory.emplace_back("Pants");
+   mainChar.inventory.emplace_back("Shoes");
 
    // sword
-   Weapon sword;
+   Weapon sword{};
    sword.attack_Power = 25;
    sword.condition = 0.5;
 
@@ -47,8 +47,9 @@ int main() {
    OldLady.Hp = 10;
    OldLady.Karma = 10;
 
+   // main game loop
    while (mainChar.Hp > 0) {
-      cout << "What type of player are you?" << endl; // initiates character type
+      cout << "What type of player are you?\n" << endl; // initiates character type
 
       cout << "At any time press any key to continue the dialogue" << endl;
 
@@ -57,17 +58,28 @@ int main() {
       cout << "Or are you the type of player who likes to sit plan and take a more thoughtful approach to the current situation" << endl;
       
       cout << "Or are you a more balanced type that likes to switch it up?" << endl;
-    
-      int i = 2;
-      int characterType;
-      do {
+
+      // TODO: infinite loop if use string
+      double characterType;
+      while (true) {
          cout << "press 1,2,3" << endl;
-      } while (cin >> characterType != 1);
+         cin >> characterType;
+         if (characterType == 1) {
+            break;
+         } else if (characterType == 2) {
+            break;
+         } else if (characterType == 3) {
+            break;
+         }
+      }
 
       //initial wake up event
       cout << "Waking up, you have no idea where you are you look around to get your bearings when...";
-	   cout << "Oh shit, a wolf comes out of nowhere!";
-	   cout << "What do you do?";
+      cout << "Oh shit, a wolf comes out of nowhere!" << endl;
+      cout << "What do you do?";
+
+      // force end of loop
+      mainChar.Hp = 0;
    };
 
    return 0;
