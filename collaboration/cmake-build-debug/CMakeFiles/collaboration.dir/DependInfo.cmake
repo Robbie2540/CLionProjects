10b85f60f43d378b1edbaaa60d2fd522c71ad483
technicolor1 <gunsofwar999@gmail.com>
2018-09-30 10:33:47 -0400

    reformat

--- collaboration/cmake-build-debug/CMakeFiles/collaboration.dir/DependInfo.cmake
+++ collaboration/cmake-build-debug/CMakeFiles/collaboration.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/jle/Documents/GitHub/CLionProjects/collaboration/main.cpp" "/Users/jle/Documents/GitHub/CLionProjects/collaboration/cmake-build-debug/CMakeFiles/collaboration.dir/main.cpp.o"
+  "/Users/jle/Documents/GitHub/simpleGame/collaboration/main.cpp" "/Users/jle/Documents/GitHub/simpleGame/collaboration/cmake-build-debug/CMakeFiles/collaboration.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- collaboration/main.cpp
+++ collaboration/main.cpp
@@ -6,95 +6,91 @@ using namespace std;
 
 // characters
 class Human {
-   public:
-   int Hp{};
-   int Karma{};
-   vector<string> inventory;
+public:
+    int Hp{};
+    int Karma{};
+    vector<string> inventory;
 };
 
 // Wolf
 class Wolf {
-   public:
-   int attack_Power;
-   int Hp;
+public:
+    int attack_Power;
+    int Hp;
 };
 
 // weapons
 class Weapon {
-   public:
-   int attack_Power;
-   double condition; // percentage
+public:
+    int attack_Power;
+    double condition; // percentage
 };
 
 int main() {
-   Wolf Wolf_lvl1{};
-   Wolf_lvl1.attack_Power = 100;
-   Wolf_lvl1.Hp = 200;
+    Wolf Wolf_lvl1{};
+    Wolf_lvl1.attack_Power = 100;
+    Wolf_lvl1.Hp = 200;
 
-   // player
-   Human mainChar;
-   mainChar.Hp = 100;
-   mainChar.Karma = 0;
-   mainChar.inventory.emplace_back("Shirt");
-   mainChar.inventory.emplace_back("Pants");
-   mainChar.inventory.emplace_back("Shoes");
+    // player
+    Human mainChar;
+    mainChar.Hp = 100;
+    mainChar.Karma = 0;
+    mainChar.inventory.emplace_back("Shirt");
+    mainChar.inventory.emplace_back("Pants");
+    mainChar.inventory.emplace_back("Shoes");
 
-   // sword
-   Weapon sword{};
-   sword.attack_Power = 25;
-   sword.condition = 0.5;
+    // sword
+    Weapon sword{};
+    sword.attack_Power = 25;
+    sword.condition = 0.5;
 
-   // old lady
-   Human OldLady;
-   OldLady.Hp = 10;
-   OldLady.Karma = 10;
+    // old lady
+    Human OldLady;
+    OldLady.Hp = 10;
+    OldLady.Karma = 10;
 
-   // main game loop
-   while (mainChar.Hp > 0) {
-      cout << "What type of player are you?\n" << endl; // initiates character type
+    // main game loop
+    while (mainChar.Hp > 0) {
+        cout << "What type of player are you?\n" << endl; // initiates character type
 
-      cout << "Are you the run and gun type that acts instantly without thinking always just in time to save the day?" << endl;
+        cout << "Are you the run and gun type that acts instantly without thinking always just in time to save the day?\n" << endl;
 
-      cout << "Or are you the type of player who likes to sit plan and take a more thoughtful approach to the current situation" << endl;
+        cout << "Or are you the type of player who likes to sit plan and take a more thoughtful approach to the current situation\n" << endl;
 
-      cout << "Or are you a more balanced type that likes to switch it up?" << endl;
+        cout << "Or are you a more balanced type that likes to switch it up?\n" << endl;
 
-      signed int characterType = 5;
-      while ( characterType != 1 & characterType != 2 & characterType != 2) {
-         cout << "press 1 if you are the run and gun type, 2 if you are the thoughtful type, or 3 if you are the balanced type." << endl;
+        signed int characterType = 5;
+        while (true) {
+            cout << "press 1 if you are the run and gun type, 2 if you are the thoughtful type, or 3 if you are the balanced type." << endl;
 
-         cin >> characterType;
-          while(cin.fail() || characterType > 3 || characterType < 1) {
-              cout << "Entry must be 1, 2, or 3." << endl;
-              cin.clear();
-              cin.ignore(256,'\n');
-              cin >> characterType;}
+            cin >> characterType;
 
+            // invalid response
+            while (cin.fail() || characterType > 3 || characterType < 1) {
+                cout << "Entry must be 1, 2, or 3" << endl;
+                cin.clear();
+                cin.ignore(256, '\n');
+                cin >> characterType;
+            }
 
-         if (characterType == 1) {
-            break;
-         }
-         else if (characterType == 2)
-         {
-            break;
-         }
-         else if (characterType == 3)
-         {
-            break;
-         }
+            if (characterType == 1) {
+                break;
+            } else if (characterType == 2) {
+                break;
+            } else if (characterType == 3) {
+                break;
+            }
+        }
 
+        //initial wake up event
+        cout << "\n";
+        cout << "Waking up, you have no idea where you are you look around to get your bearings when...";
+        cout << "Oh shit, a wolf comes out of nowhere!" << endl;
+        cout << "What do you do?";
 
+        // force end of game loop
+        mainChar.Hp = 0;
+    };
 
-      }
-
-      //initial wake up event
-      cout << "Waking up, you have no idea where you are you look around to get your bearings when...";
-      cout << "Oh shit, a wolf comes out of nowhere!" << endl;
-      cout << "What do you do?";
-
-      // force end of loop
-      mainChar.Hp = 0;
-   };
-
-   return 0;
+    return 0;
 };
